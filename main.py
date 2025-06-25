83aaa626929c7509b75cc1fd87674e54c3529e5e
phutharesuanwachirapisut <phutharesuan.wachirapisut@gmail.com>
2025-06-25 11:00:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,4 +48,6 @@ def predict():
 
 # 🏁 Run the app (for local testing only)
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
